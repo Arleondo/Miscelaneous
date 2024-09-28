6d994f59e1237d67647e3651c88bc754847fd894
El_INSANO <arnolsmith.tacca@ucsp.edu.pe>
2024-09-27 23:32:47 -0500

    Trying to print someonething?
    Intento de uso del nuevo print  (fallido)

---
+++ Print_new/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <print>
+using namespace std;
+
+int main()
+{
+
+    // new function
+    print("Hello World");
+
+    return 0;
+}
--- Stackover_27-09/main.cpp
+++ Stackover_27-09/main.cpp
@@ -1,56 +1,68 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-class Persona{
-public:
-    Persona (int _edad, string _nombre, string _apellido){
-        edad=_edad;
-        nombre=_nombre;
-        apellido=_apellido;
-    }
-    void veredad(){
-        cout<<edad<<endl;
-    }
-    void vernombre(){
-        cout<<nombre<<endl;
-    }
-    void verapellido(){
-        cout<<apellido<<endl;
-    }
-    int edad;
-    string nombre;
-    string apellido;
-};
+int main(){
+    //Variables
+    int nro;
+    int factor = 0;
+    int factorizar = 1;
+    int factormin = 0;
+    int factormax = 0;
+    //char continua;
+    char continua = 's';
 
-class dni : Persona{
-public:
-    dni(string _calle, int _altura, int _documento, char _genero, const Persona& x) : Persona(x.edad,x.nombre,x.apellido){
-        calle=_calle;
-        altura=_altura;
-        documento=_documento;
-        genero=_genero;
+
+    //Ingreso de datos
+
+
+
+    do {
+        cout <<"Desea ingresar un número entero positivo para factorizar  (s/S - n/N):  ";
+        cin >> continua;
+
+        cout <<"Ingrese el número a factorizar: ";
+        cin >> nro;
+
+
+
+        if(nro >= 0){
+            //factorizar
+            for (int i=1; i<=nro; i++){
+                factorizar *= i;
+            }
+
+
+            //busca el máximo y el mínimo
+            if (factorizar > factormax){
+                factormax = factorizar;
+            } else if (factorizar < factormin){
+                factormin = factorizar;
+            }
+
+
+            //nostrar el resultado
+            cout <<"El resultado de !"<< nro <<" es: " << factorizar << endl;
+
+        } else if (nro == 0){
+            cout << "El resultado de !0 es: 1" << endl;
+            //return (0);
+        }
+        else
+            cout << "Número inválido, intente nuevamente" << endl;
+
+
+
+    } while (continua == 's' || continua == 'S');
+
+
+    if (continua == 'n' || continua == 'N'){
+        //mostrar maximo y minimo
+        cout << "El número mas chico obtenido es: "<<factormin << endl;
+        cout << "El número mas alto obtenido es: "<<factormax << endl;
     }
 
-    void mostrardoc(){
-        cout<<"El documento nro: "<<documento<<" .Pertenece a "<<nombre<<" "<<apellido<<" de "<<edad<<" años"<<endl;
-        cout<<"Vive en "<<calle<<" .Altura "<<altura<<endl;
-        cout<<"Es de genero "<<genero<<endl;
-    }
-private:
-    string calle;
-    int altura;
-    int documento;
-    char genero;
-};
-int main () {
-    int edad; cin>>edad;
-    string nombre,apellido; cin>>nombre>>apellido;
-    Persona per(edad,nombre,apellido);
-    string calle;   cin>>calle;
-    int altura,documento;   cin>>altura>>documento;
-    char genero;    cin>>genero;
-    dni dni1(calle,altura,documento,genero,per);
-    dni1.mostrardoc();
+
+    //mostrar maximo y minimo
+    //getch ();
     return 0;
 }
